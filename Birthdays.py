413f40d7b5f96adfb41ab5d9ecb267d87110725f
DeveloperSaleha <yusra.saleha031@gmail.com>
2024-09-06 14:27:39 +0500

    Add files via upload

---
+++ Birthdays.py
@@ -0,0 +1,29 @@
+name1 = "Anna:"
+print(name1)
+
+birthday1 = "12th Feb 1990"
+print(birthday1)
+
+name2 = "John:"
+print(name2)
+
+birthday2 = "3rd Jan 2015"
+print(birthday2)
+
+name3 = "Barbara:"
+print(name3)
+
+birthday3 = "1st April 2023"
+print(birthday3)
+
+name4 = "Ariel:"
+print(name4)
+
+birthday4 = "5th May 2019"
+print(birthday4)
+
+name5 = "Raymond:"
+print(name5)
+
+birthday5 = "29th nov 2014"
+print(birthday5)
