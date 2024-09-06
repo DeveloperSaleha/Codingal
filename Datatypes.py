31adccc6f0e1ff380154f9e9b9e49cc21ea606c0
DeveloperSaleha <yusra.saleha031@gmail.com>
2024-09-06 21:43:31 +0500

    Add files via upload

---
+++ Datatypes.py
@@ -0,0 +1,56 @@
+#data types - > int, float, boolean, string
+
+a=2024
+print(a) 
+print(type(a))
+
+x=float(a)
+print(type(x))
+
+b=0.56
+print(b)
+print(type(b))
+
+y=int(b)
+print(type(y))
+
+c=True
+print(c)
+print(type(c))
+
+d="saleha"
+print(d)
+print(type(d))
+
+#v=int(d)
+#print(type(v))
+
+
+#String operations 
+
+#1: String concatenation(+) - Add one string with another string or with a number 
+
+name1="Codingal"
+name2="Technology"
+name3=name1+" "+name2
+print(name3)
+
+name4="Jonathan"
+name5="Will"
+name6=name4+" "+name5
+print(name6)
+
+str="Saleha"
+print(str[::5])
+print(str[0::1])
+print(str[::-1])
+print(str[1:4:1])
+
+
+#taking input from the user
+
+name = input("Please enter your name : ")
+age = input("Please type your age : ")
+
+
+
