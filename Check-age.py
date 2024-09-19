55cda70b2dcfcb67310da6824515c0ad3bfa2cf1
DeveloperSaleha <yusra.saleha031@gmail.com>
2024-09-19 16:25:07 +0500

    Add files via upload

---
+++ Check-age.py
@@ -0,0 +1,5 @@
+age = input("Enter your age: ")
+if(age >= "10" and age <= "20"):
+    print("You are able to enrol in this class.")
+else:
+    print("You are not able to enrol.")
---
+++ Summer-times.py
@@ -0,0 +1,19 @@
+month = int(input("Enter month: "))
+if month == 3 or month == 4 or month == 5:
+    print("it is summer.")
+elif month == 1 or month == 2 or month == 6:
+    print("it is  not summer.")
+elif month == 7 or month == 8 or month == 9:
+    print("it is  not summer.")
+elif month == 10 or month == 11 or month == 12:
+    print("it is  not summer.")
+
+
+
+
+
+
+
+
+
+
